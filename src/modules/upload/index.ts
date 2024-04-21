9902ee09fb11783746954f09387733603d500f78
Leeyah-123 <junaidaaliyah260@gmail.com>
2024-04-21 12:49:00 +0100

    feat: Implement deletion of uploaded image

--- src/modules/upload/index.ts
+++ src/modules/upload/index.ts
@@ -1,11 +1,23 @@
 import { Router } from 'express';
+import { authMiddleware } from '../../middlewares/auth';
 import { upload } from '../../middlewares/upload';
 import UploadController from './upload.controller';
 
 const router = Router();
 const Controller = new UploadController();
 
-router.post('/image', upload.single('image'), Controller.uploadImage);
-router.post('/images', upload.array('images'), Controller.uploadImages);
+router.post(
+  '/image',
+  authMiddleware,
+  upload.single('image'),
+  Controller.uploadImage
+);
+router.post(
+  '/images',
+  authMiddleware,
+  upload.array('images'),
+  Controller.uploadImages
+);
+router.delete('/:url', authMiddleware, Controller.deleteImage);
 
 export default router;
